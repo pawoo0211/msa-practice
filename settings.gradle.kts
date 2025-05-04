987a5603e7d97e2fd9f3ede9cd07f01f0cf1de48
pawoo0211 <pawoo0211@naver.com>
2025-05-04 16:47:02 +0900

    인프라 설정 모듈, 어플리케이션 모듈 구조 분리

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
 rootProject.name = "ecommerce"
 
 include("apps")
-include("apps:discovery-service")
-include("apps:user-service")
+include("apps:cloud-hub")
+include("apps:cloud-hub:discovery-service")
+include("apps:cloud-hub:gateway-service")
+include("apps:application")
+include("apps:application:user-service")
+include("apps:application:first-service")
+findProject(":apps:application:first-service")?.name = "first-service"
+include("apps:application:second-service")
+findProject(":apps:application:second-service")?.name = "second-service"

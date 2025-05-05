5663ca0055e6317a095bf2911bd2895c4ccbaf66
pawoo0211 <pawoo0211@naver.com>
2025-05-05 11:54:01 +0900

    feat: second-service 모듈 요청 및 응답 header 추가

--- apps/application/second-service/src/main/kotlin/com/example/second/presentation/Controller.kt
+++ apps/application/second-service/src/main/kotlin/com/example/second/presentation/Controller.kt
@@ -1,14 +1,23 @@
 package com.example.second.presentation
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/second-service")
 class Controller {
-    @GetMapping("welcome")
+    @GetMapping("/welcome")
     fun healthCheck(): String {
         return "Welcome to the Second Service"
+    }
+
+    @GetMapping("/message")
+    fun checkSecondRequestHeader(
+        @RequestHeader(value = "second-request") header: String
+    ): String {
+        println("second-request : ${header}")
+        return "OK"
     }
 }

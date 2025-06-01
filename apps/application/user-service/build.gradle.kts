d7f75193dd5365d23e3db8c8f2833bef3a969710
pawoo0211 <pawoo0211@naver.com>
2025-06-01 23:37:10 +0900

    feat: config 서버로부터 회원 yaml 적용(#15)

--- apps/application/user-service/build.gradle.kts
+++ apps/application/user-service/build.gradle.kts
@@ -27,8 +27,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     implementation("org.modelmapper:modelmapper:3.0.0")
 
     implementation("com.h2database:h2:2.3.232")

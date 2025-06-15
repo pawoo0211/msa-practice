46f849e925f76b76e08f586d360d2b1735a4f004
pawoo0211 <pawoo0211@naver.com>
2025-06-15 15:59:30 +0900

    feat: 주문에 따른 상품 수량 업데이트 구현(#17)

--- apps/application/catalog-service/src/main/kotlin/com/example/catalog/application/service/CatalogService.kt
+++ apps/application/catalog-service/src/main/kotlin/com/example/catalog/application/service/CatalogService.kt
@@ -4,4 +4,5 @@ import com.example.catalog.application.dto.response.FindCatalogResponse
 
 interface CatalogService {
     fun findAllCatalog(): List<FindCatalogResponse>
+    fun updateQuantity(kafkaMessage: String)
 }
--- apps/application/catalog-service/src/main/kotlin/com/example/catalog/application/service/CatalogServiceImpl.kt
+++ apps/application/catalog-service/src/main/kotlin/com/example/catalog/application/service/CatalogServiceImpl.kt
@@ -2,6 +2,8 @@ package com.example.catalog.application.service
 
 import com.example.catalog.application.dto.response.FindCatalogResponse
 import com.example.catalog.domain.CatalogRepository
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,6 +18,26 @@ class CatalogServiceImpl(
                 stock = it.stock,
                 unitPrice = it.unitPrice
             )
+        }
+    }
+
+    override fun updateQuantity(kafkaMessage: String) {
+        var map = mutableMapOf<Any, Any>()
+        val mapper = ObjectMapper()
+
+        try {
+            val map: Map<String, Any> = mapper.readValue(kafkaMessage, object : TypeReference<Map<String, Any>>() {})
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        val productId = map.get("productId").toString()
+        val catalogEntity = catalogRepository.findByProductId(productId)
+
+        if (catalogEntity != null) {
+            val quantity = (map["quantity"].toString()).toIntOrNull() ?: 0
+            catalogEntity.stock = catalogEntity.stock - quantity
+            catalogRepository.save(catalogEntity)
         }
     }
 }
